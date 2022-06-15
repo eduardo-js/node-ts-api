d9d65cf8d356ccc1e4be571bb9e44ad2c5108c47
José Eduardo dos Santos <jose.eduardo.ds@gmail.com>
2022-06-14 22:07:01 -0300

    chore: base express config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World')
+import 'dotenv/config';
+import express from 'express';
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(process.env.PORT)
+
+
+
