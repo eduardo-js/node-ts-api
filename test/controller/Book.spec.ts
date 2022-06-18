8f7b923b8796778e56418361ad7d61a975cec182
José Eduardo dos Santos <jose.eduardo.ds@gmail.com>
2022-06-17 22:23:58 -0300

    test: proper controller testing

--- test/controller/Book.spec.ts
+++ test/controller/Book.spec.ts
@@ -1,32 +1,39 @@
-import {ResourceNotFound} from '../../src/error/Http';
-import BookRepository from '../../src/repository/Book';
+import request from 'supertest';
+import {ResourceNotFound, ValidationError} from '../../src/error/Http';
 import BookService from '../../src/service/Book';
 import {bookMock} from '../mock/Book.mock';
+import container from '../../src/shared/container';
+import App from '../../src/app';
 
-jest.mock('../../src/repository/Book');
-describe('BookService', () => {
-  const bookRepository = jest.mocked(BookRepository.prototype);
+jest.mock('../../src/service/Book');
+
+describe('BookController', () => {
+  const app = container.resolve(App).app;
+  const bookService = jest.mocked(BookService.prototype);
   afterEach(() => {
     jest.clearAllMocks();
   });
   describe('getBookById', () => {
     it('should return 200 when book exists', async () => {
-      bookRepository.getBookById.mockResolvedValueOnce(bookMock);
-      const sut = await new BookService(bookRepository).getBookById(1);
+      const data = {book: bookMock};
+      bookService.getBookById.mockResolvedValue({status: 200, data});
+      const sut = await request(app).get('/api/book/v1/1');
       expect(sut.status).toBe(200);
-      expect(sut.data).toStrictEqual({book: bookMock});
+      expect(sut.body).toStrictEqual(data);
     });
     it('should return 404 when book !exists', async () => {
-      bookRepository.getBookById.mockResolvedValueOnce(null);
-      const sut = await new BookService(bookRepository).getBookById(1);
+      bookService.getBookById.mockResolvedValue({
+        status: ResourceNotFound.status,
+        data: ResourceNotFound.data,
+      });
+      const sut = await request(app).get('/api/book/v1/1');
       expect(sut.status).toBe(ResourceNotFound.status);
-      expect(sut.data).toStrictEqual(ResourceNotFound.data);
+      expect(sut.body).toStrictEqual(ResourceNotFound.data);
     });
-    it('should return 400 when book id <= 0 ', async () => {
-      bookRepository.getBookById.mockResolvedValueOnce(null);
-      const sut = await new BookService(bookRepository).getBookById(0);
-      expect(sut.status).toBe(ResourceNotFound.status);
-      expect(sut.data).toStrictEqual(ResourceNotFound.data);
+    it('should return 400 on id <= 0', async () => {
+      const sut = await request(app).get('/api/book/v1/-1');
+      expect(sut.status).toBe(ValidationError.status);
+      expect(sut.body).toHaveProperty('name', 'ZodError');
     });
   });
 });
