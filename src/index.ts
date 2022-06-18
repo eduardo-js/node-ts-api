527a10fef2de64e19b206f3ce08bf5266b433274
José Eduardo dos Santos <jose.eduardo.ds@gmail.com>
2022-06-17 22:04:09 -0300

    refactor: server init

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import App from './app';
 import container from './shared/container';
 import config from './config';
 
-try {
-  container.resolve(App).app.listen(config.app.port || 3000);
-  console.log(`Server is running on port ${config.app.port || 3000}`);
-} catch (error) {
-  console.info(error);
-}
+const main = () => {
+  try {
+    container.resolve(App).app.listen(config.app.port || 3000);
+    console.log(`Server is running on port ${config.app.port || 3000}`);
+  } catch (error) {
+    console.info(error);
+  }
+};
+
+void main();
