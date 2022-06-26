e3b1d954f22dd893c303ae2c56c5c3a134a2dfd3
José Eduardo dos Santos <jose.eduardo.ds@gmail.com>
2022-06-26 19:09:50 -0300

    chore: request body

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import {ErrorHandler} from './middleware/ErrorHandler';
 import 'express-async-errors';
 import Routes from './routes';
-const app = express();
 
+const app = express();
+app.use(express.json());
 app.use(Routes);
 app.use(ErrorHandler);
 
