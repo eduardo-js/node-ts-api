7d4e13c59f9097ae0b1449a87a47aad4ceb6c15b
José Eduardo dos Santos <jose.eduardo.ds@gmail.com>
2022-06-26 19:14:52 -0300

    test: adding memory repository and createBook

--- test/mock/BookRepository.Memory.ts
+++ test/mock/BookRepository.Memory.ts
@@ -1,5 +1,7 @@
 import {Book} from '@prisma/client';
+import {randomUUID} from 'crypto';
 import {IBookRepository} from '../../src/repository/Book';
+import {CreateBookParams} from '../../src/shared/validators/Book';
 
 export class BookRepositoryMemory implements IBookRepository {
   books: Book[] = [];
@@ -7,8 +9,13 @@ export class BookRepositoryMemory implements IBookRepository {
     return this.books.find((book) => book.id === id) || null;
   }
 
-  async createBook(book: Book) {
+  async createBook(data: CreateBookParams) {
+    const book = {
+      id: randomUUID(),
+      ...data,
+    };
     this.books.push(book);
+    return book;
   }
   async reset() {
     this.books = [];
